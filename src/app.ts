ed489440022df838d40d2ca773dae79ffe665618
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-07-21 20:12:51 -0300

    testing

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { config } from "./data/config";
 import { AuthRouter } from "./routes/Auth";
 import { RecipeRouter } from "./routes/Recipe";
 import { UserRouter } from "./routes/User";
-import cors from "cors";
 
 const app = express();
 
@@ -14,20 +13,13 @@ if (!config().port || !config().accessToken || !config().refreshToken) {
 
 app.use(express.json());
 
-app.use(
-	cors({
-		origin: true, // Allow requests from any origin
-		credentials: true, // Allow credentials (cookies, HTTP authentication) to be included in requests
-		exposedHeaders: ["Authorization"], // Expose the Authorization header to the frontend
-	})
-);
-
 app.use("/auth", AuthRouter);
 app.use("/recipes", RecipeRouter);
 app.use("/user", UserRouter);
 
 const server = app.listen(3001, () => {
 	console.log("Server is running");
+	console.log("DATABASE_URL: ", process.env.DATABASE_URL);
 });
 
 server.keepAliveTimeout = 120 * 1000;
