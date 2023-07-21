2600673e094500cabab54469132268ac2efd42f9
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-07-21 16:48:52 -0300

    auth error :(

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -7,3 +7,5 @@ export const AuthRouter = Router();
 AuthRouter.post("/login", AuthValidator.login, AuthController.login);
 AuthRouter.post("/register", AuthValidator.register, AuthController.register);
 AuthRouter.post("/refresh-token", AuthValidator.refreshToken, AuthController.refreshToken);
+
+//
