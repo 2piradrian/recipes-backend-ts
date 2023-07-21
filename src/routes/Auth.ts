30dc58236718a22f8b2ee0a8db2a2a66d0ee0e33
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-07-21 17:12:26 -0300

    build

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -7,5 +7,3 @@ export const AuthRouter = Router();
 AuthRouter.post("/login", AuthValidator.login, AuthController.login);
 AuthRouter.post("/register", AuthValidator.register, AuthController.register);
 AuthRouter.post("/refresh-token", AuthValidator.refreshToken, AuthController.refreshToken);
-
-//
